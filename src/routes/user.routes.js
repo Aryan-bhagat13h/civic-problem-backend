08972c83ce42653a8d214316f7ca16162b25db52
Aryan-bhagat13h <aryanbhagat228@gmail.com>
2026-09-07 07:59:59 +0530

    feat: add change password and update profile routes to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser } from "../controller/user.controller.js"
+import { 
+  registerUser, 
+  loginUser, 
+  logoutUser, 
+  changePassword, 
+  updateProfile 
+} from "../controller/user.controller.js"
 import { verifyJwt } from "../middlwears/auth.middlerware.js"
 
 const router = Router()
@@ -7,5 +13,7 @@ const router = Router()
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt, logoutUser)
+router.route("/change-password").post(verifyJwt, changePassword)
+router.route("/update-profile").patch(verifyJwt, updateProfile)
 
 export default router
